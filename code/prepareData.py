55afd0dbe9dc2b67c2a9d97e14141d08128fa9a8
zhonghaoxi <zhonghaoxi@yeah.net>
2019-03-27 22:51:46 +0800

    pep8 fixed

--- code/prepareData.py
+++ code/prepareData.py
@@ -1,12 +1,13 @@
 import random
-f = open('graph.txt','rb')
+
+f = open('graph.txt', 'rb')
 edges = [i for i in f]
-selected = random.sample(edges,int(len(edges)*0.15))
+selected = random.sample(edges, int(len(edges) * 0.15))
 remain = [i for i in edges if i not in selected]
-fw1 = open('graph.txt','wb')
-fw2 = open('test_graph.txt','wb')
+fw1 = open('graph.txt', 'wb')
+fw2 = open('test_graph.txt', 'wb')
 
 for i in selected:
-	fw1.write(i)
+    fw1.write(i)
 for i in remain:
-	fw2.write(i)
+    fw2.write(i)
