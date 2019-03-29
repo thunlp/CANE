733c602a7a185247e48c9de4b1cccba88ba20353
zhonghaoxi <zhonghaoxi@yeah.net>
2019-03-29 20:45:16 +0800

    fix a bug

--- code/prepareData.py
+++ code/prepareData.py
@@ -10,8 +10,9 @@ args = parser.parse_args()
 
 f = open('../datasets/%s/graph.txt' % args.dataset, 'rb')
 edges = [i for i in f]
-selected = random.sample(edges, int(len(edges) * float(args.ratio)))
+selected = int(len(edges) * float(args.ratio))
 selected = selected - selected % config.batch_size
+selected = random.sample(edges, selected)
 remain = [i for i in edges if i not in selected]
 fw1 = open('temp/graph.txt', 'wb')
 fw2 = open('temp/test_graph.txt', 'wb')
